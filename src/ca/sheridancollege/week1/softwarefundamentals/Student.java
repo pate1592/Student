b4f4f3d152f2302ab9e54e15d25a5d4743209b30
pate1592 <pate1592@sheridancollege.ca>
2020-01-22 09:53:14 -0500

    first commit

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -12,7 +12,7 @@ package ca.sheridancollege.week1.softwarefundamentals;
 public class Student {
     
     private String name;
-    private String startus;
+    private String start;
 
     /**
      * Get the value of name
